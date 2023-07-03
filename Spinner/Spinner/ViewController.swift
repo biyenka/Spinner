8605e39ce312e1fa3bd3108fcfc29b82358c5a56
biyenka <135021588+biyenka@users.noreply.github.com>
2023-07-03 16:41:17 +0200

    Bug fixed

--- Spinner/Spinner/ViewController.swift
+++ Spinner/Spinner/ViewController.swift
@@ -11,11 +11,35 @@ class ViewController: UIViewController {
         
         createCircle(startAngle: 20, endAngle: 90)
         createArrow()
-        startCheckingIntersection()
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(didTapView(_:)))
+        tapGestureRecognizer.numberOfTapsRequired = 1
+        self.view.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    @objc func didTapView(_ sender: UITapGestureRecognizer) {
+        _ = sender.location(in: self.view)
+        
+        guard let presentationLayer = arrow.presentation() else {
+            return
+        }
+        
+        let arrowPath = UIBezierPath(rect: presentationLayer.frame)
+        let segmentPath = UIBezierPath(cgPath: segmentLayer.path!)
+        
+        if arrowPath.cgPath.intersects(segmentPath.cgPath) {
+            if !isIntersecting {
+                isIntersecting = true
+                print("Попало!")
+            }
+        } else {
+            isIntersecting = false
+            print("Мимо")
+        }
     }
     
     private func createSegment(startAngle: CGFloat, endAngle: CGFloat) -> UIBezierPath {
-        return UIBezierPath(arcCenter: CGPoint(x: self.view.frame.midX, y: self.view.frame.midY), radius: 150, startAngle: startAngle.toRadians(), endAngle: endAngle.toRadians(), clockwise: true)
+        return UIBezierPath(arcCenter: CGPoint(x: self.view.frame.midX, y: self.view.frame.midY), radius: Double.random(in: 80...150), startAngle: startAngle.toRadians(), endAngle: endAngle.toRadians(), clockwise: true)
     }
     
     private func createCircle(startAngle: CGFloat, endAngle: CGFloat) {
@@ -25,12 +49,13 @@ class ViewController: UIViewController {
         segmentLayer.lineWidth = 30
         segmentLayer.strokeColor = UIColor.blue.cgColor
         segmentLayer.fillColor = UIColor.clear.cgColor
+        segmentLayer.lineCap = .round
         
         self.view.layer.addSublayer(segmentLayer)
     }
     
     private func createArrow() {
-        arrow.bounds = CGRect(x: 0, y: 0, width: 10, height: 200)
+        arrow.bounds = CGRect(x: 0, y: 0, width: 7, height: 200)
         arrow.position = view.center
         arrow.backgroundColor = UIColor.red.cgColor
         arrow.anchorPoint = CGPoint(x: 0.5, y: 0.9)
@@ -44,39 +69,10 @@ class ViewController: UIViewController {
         let rotationAnimation = CABasicAnimation(keyPath: "transform.rotation.z")
         rotationAnimation.fromValue = 0
         rotationAnimation.toValue = 2 * Double.pi
-        rotationAnimation.duration = 5 // Длительность анимации в секундах
+        rotationAnimation.duration = 3
         rotationAnimation.repeatCount = .infinity
         
         arrow.add(rotationAnimation, forKey: "secondHandAnimation")
-    }
-    
-    private func startCheckingIntersection() {
-        let displayLink = CADisplayLink(target: self, selector: #selector(checkIntersection))
-        displayLink.add(to: .current, forMode: .common)
-    }
-    
-    @objc private func checkIntersection() {
-        guard let presentationLayer = arrow.presentation() else {
-            return
-        }
-        
-        let arrowPath = UIBezierPath(rect: presentationLayer.frame)
-        let segmentPath = UIBezierPath(cgPath: segmentLayer.path!)
-        
-        if arrowPath.cgPath.intersects(segmentPath.cgPath) {
-            if !isIntersecting {
-                isIntersecting = true
-                print("Стрелка пересекает сегмент")
-            }
-        } else {
-            isIntersecting = false
-        }
-    }
-}
-
-extension ViewController: CAAnimationDelegate {
-    func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
-        checkIntersection()
     }
 }
 
